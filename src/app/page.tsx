28f473aa60526038c99634eed9fa9693f294f2d9
zikrya <zikrya8@gmail.com>
2025-03-22 23:42:14 -0400

    feat: added componets to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
+import Flavors from "@/components/Flavors";
+import ProductContent from "@/components/ProductContent";
+import ProductGallery from "@/components/ProductGallery";
+
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-
-    </div>
-  );
+    <main className="min-h-screen p-8">
+      <h1 className="text-2xl font-bold mb-6">Build your Bundle [Desktop]</h1>
+      <div className="flex flex-col md:flex-row gap-8">
+        <ProductGallery />
+        <div className="flex flex-col gap-6">
+          <ProductContent />
+          <Flavors />
+        </div>
+      </div>
+    </main>
+  )
 }
+
+
